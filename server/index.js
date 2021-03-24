0e17467aed43c6d76c7bc9363b5191ef4d14e706
Nathan Kawaller <kawaller@gmail.com>
2021-03-24 02:53:55 -0400

    set up post routes

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
