9598868a53288eb0e14a36c6d6a76ef5cb173443
Nathan Kawaller <kawaller@gmail.com>
2021-03-25 02:47:47 -0400

    post component setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,14 @@ import React from "react";
 import { Container } from "@material-ui/core";
 
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 const App = () => {
   return (
     <div>
-      <Container
-        maxWidth="lg"
-        disableGutters={true}
-      >
+      <Container maxWidth="lg" disableGutters={true}>
         <Navbar />
+        <Home />
       </Container>
     </div>
   );
