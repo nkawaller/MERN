c14d8a885aa3c969db6ae44f87baf114b112ebb5
Nathan Kawaller <kawaller@gmail.com>
2021-03-24 21:01:12 -0400

    started navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Container } from "@material-ui/core";
+
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
-    return (
-        <div>
-            <h1>React App</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Container
+        maxWidth="lg"
+        disableGutters={true}
+        style={{ background: "palevioletred" }}
+      >
+        <Navbar />
+      </Container>
+    </div>
+  );
+};
 
 export default App;
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppBar, Typography, Toolbar, Button } from "@material-ui/core";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Typography variant='h2' align="center">NavBar Component</Typography>
+    </div>
+  );
+};
+
+export default Navbar;
