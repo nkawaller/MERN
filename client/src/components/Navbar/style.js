59d47212453fc3701a8a435821921b5a774a1029
Nathan Kawaller <kawaller@gmail.com>
2021-03-25 02:10:16 -0400

    removed dead code

--- client/src/components/Navbar/style.js
+++ client/src/components/Navbar/style.js
@@ -1,35 +1,33 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-// Pass theme into the callback if you want to use purple
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
-    appBar: {
-        // background: 'palevioletred',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '10px 50px'
-    },
-    heading: {
-        color: 'black',
-    },
-    toolbar: {
-        // background: 'papayawhip',
-        display: 'flex',
-        justifyContent: 'flex-end',
-    },
-    profile: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        width: '300px'
-    },
-    userName: {
-        display: 'flex',
-        alignItems: 'center',
-    },
-    brandContainer: {
-        display: 'flex',
-        alignItems: 'center'
-    }
-}))
+  appBar: {
+    // background: 'palevioletred',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "10px 50px",
+  },
+  heading: {
+    color: "black",
+  },
+  toolbar: {
+    // background: 'papayawhip',
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  profile: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "300px",
+  },
+  userName: {
+    display: "flex",
+    alignItems: "center",
+  },
+  brandContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+}));
