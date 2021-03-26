d7f4917eb5c3d929d9f738e1de87497392d618ad
Nathan Kawaller <kawaller@gmail.com>
2021-03-26 00:03:47 -0400

    create post action

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,4 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://localhost:5000'});
 
 export const fetchPosts = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts');
