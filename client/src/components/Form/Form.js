c8d4f44ce409f49bddf7adf714b0db9221416235
Nathan Kawaller <kawaller@gmail.com>
2021-03-26 00:00:10 -0400

    base64 setup

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -15,6 +15,9 @@ const Form = () => {
     const handleSubmit = () => {
 
     }
+    const clear = () => {
+        
+    }
     return (
             // <Paper>
             //     <Typography variant="h6">
@@ -29,9 +32,9 @@ const Form = () => {
                     <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value})}/>
                     <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
                     <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })}/>
-                    <div className={classes.fileInput}><FileBase></FileBase></div>
+                    <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })} /></div>
                     <Button className={classes.buttonSubmit} variant="outlined" color="default" size="large" type="submit" fullWidth>Submit</Button>
-                    <Button variant="outlined" color="default" size="small" fullWidth>Clear</Button>
+                    <Button variant="outlined" color="default" size="small" onClick={clear} fullWidth>Clear</Button>
                 </form>
             </Paper>
     )
