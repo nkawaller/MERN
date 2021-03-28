6f276aedebf673a3250823c90c108ce4fd2e9de7
Nathan Kawaller <kawaller@gmail.com>
2021-03-28 02:55:58 -0400

    fixed container error

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Typography, Toolbar, Button } from "@material-ui/core";
 import { useDispatch } from 'react-redux';
 import useStyles from './style'
 
 const Navbar = () => {
-  const classes = useStyles();
-  // const dispatch = useDispatch();
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+const classes = useStyles();
+const history = useHistory();
+const location = useLocation();
+const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+const dispatch = useDispatch();
 
+const logout = () => {
+  dispatch({ type: 'LOGOUT'})
+  history.pushState('/')
+  setUser(null);
+}
 useEffect(() => {
-  const token = user?.token;
+  // const token = user?.token;
   setUser(JSON.parse(localStorage.getItem('profile')))
-}, [])
+}, [location])
 
   return (
     <AppBar data-testid='navbar' className={classes.appBar} position="static" color="transparent" elevation={0}>
@@ -25,7 +32,7 @@ useEffect(() => {
         {user ? (
         <div className={classes.profile}>
         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-        <Button  variant="outlined">Log In</Button>
+        <Button variant="outlined" className={classes.logout} color="default" onClick={logout}>Log Out</Button>
         </div>
         ) : (
         <Button component={Link} to='/auth' variant="outlined">Log In</Button>
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,9 +5,11 @@ const authReducer  = (state = { authData: null }, action) => {
         case AUTH:
             localStorage.setItem('profile', JSON.stringify({ ...action?.data}))
             return { ...state, authData: action?.data };
-    
+        case LOGOUT:
+            localStorage.clear();
+            return { ...state, authData: null}
         default:
-            break;
+            return state;
     }
 }
 
