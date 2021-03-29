38f1c98527fc05cde6378a597cc09e78830dd14a
Nathan Kawaller <kawaller@gmail.com>
2021-03-29 03:50:30 -0400

    added token expiration logout

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Typography, Toolbar, Button, Avatar } from "@material-ui/core";
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode'
 import useStyles from './style'
 
 const Navbar = () => {
@@ -21,7 +22,13 @@ const logout = () => {
 }
 
 useEffect(() => {
-  // const token = user?.token;
+  const token = user?.token;
+
+  if(token) {
+    const decodedToken = decode(token);
+
+    if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+  }
   setUser(JSON.parse(localStorage.getItem('profile')))
 }, [location])
 
