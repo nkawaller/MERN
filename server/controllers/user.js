7df04a1a2f3b969e518b9ce31ad37397dc97638b
Nathan Kawaller <kawaller@gmail.com>
2021-03-28 20:30:03 -0400

    user model

---
+++ server/controllers/user.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import User from '../models/user.js'
+
+export const signin = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const signup = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
