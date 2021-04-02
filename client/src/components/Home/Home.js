c2c0239f2895823303747bafd1b71c44f88654db
Nathan Kawaller <kawaller@gmail.com>
2021-04-02 03:08:33 -0400

    formatted Home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,7 +6,6 @@ import Form from "../Form/Form";
 import Posts from "../Posts/Posts";
 
 const Home = () => {
-
   const [currentId, setCurrentId] = useState(0);
   const dispatch = useDispatch();
 
@@ -17,12 +16,17 @@ const Home = () => {
   return (
     <Grow in>
       <Container>
-        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
           <Grid item xs={12} sm={7}>
-            <Posts setCurrentId={setCurrentId}/>
+            <Posts setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Form currentId={currentId} setCurrentId={setCurrentId}/>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
           </Grid>
         </Grid>
       </Container>
